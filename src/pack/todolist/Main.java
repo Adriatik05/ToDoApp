5778400c229a85884c3bf19466792051985ece3c
adriatik rexhepi <aadriatikk@gmail.com>
2021-01-08 21:34:48 +0100

    first commit

---
+++ src/pack/todolist/Main.java
@@ -0,0 +1,62 @@
+package pack.todolist;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static String filename = "tasks.obj";
+
+    public static void main(String[] args) {
+
+        TodoList todoList = new TodoList();
+
+
+        String menuChoice = "-17";
+
+        try {
+            Scanner input = new Scanner(System.in);
+
+
+            todoList.readFromFile(filename);
+
+            Messages.showMessage("Welcome to ToDoList", false);
+
+            while (!menuChoice.equals("4")) {
+                Messages.mainMenu(todoList.notCompletedCount(), todoList.completedCount());
+                menuChoice = input.nextLine();
+
+                switch (menuChoice) {
+                    case "1":
+                        Messages.listAllTasksMenu();
+                        todoList.listAllTasks(input.nextLine());
+                        break;
+                    case "2":
+                        todoList.readTaskFromUser();
+                        break;
+                    case "3":
+                        todoList.listAllTasksWithIndex();
+                        Messages.editTaskSelection();
+                        todoList.editTask(input.nextLine());
+                        break;
+                    case "4":
+                        break;
+
+                    default:
+                        Messages.unknownMessage();
+                }
+            }
+
+          
+            todoList.saveToFile(filename);
+            Messages.byeMessage();
+
+        } catch (Exception e) {
+            Messages.showMessage("UNCAUGHT EXCEPTION THROWN", true);
+            System.out.println("Trying to write the unsaved data of all tasks in data file");
+            todoList.saveToFile(filename);
+            System.out.println(e.getMessage());
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+    }
+}
